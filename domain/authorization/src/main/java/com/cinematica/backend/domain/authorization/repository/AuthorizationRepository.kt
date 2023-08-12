9db833dd79ce7fd132d1b60fdf3aa5ffe75f1698
vadwwxz <vadwwxz@gmail.com>
2023-08-12 21:10:16 +0300

    create routing for receive authorization state (2.0)

--- domain/authorization/src/main/java/com/cinematica/backend/domain/authorization/repository/AuthorizationRepository.kt
+++ domain/authorization/src/main/java/com/cinematica/backend/domain/authorization/repository/AuthorizationRepository.kt
@@ -1,8 +1,12 @@
 package com.cinematica.backend.domain.authorization.repository
 
 import com.cinematica.backend.domain.authorization.types.signup.SignUpData
+import com.cinematica.backend.domain.authorization.types.state.AuthorizationState
+import com.cinematica.backend.domain.authorization.types.state.AuthorizationStateData
 
 interface AuthorizationRepository {
 
     suspend fun signUp(signUpData: SignUpData)
+
+    suspend fun state(authorizationStateData: AuthorizationStateData): AuthorizationState
 }
