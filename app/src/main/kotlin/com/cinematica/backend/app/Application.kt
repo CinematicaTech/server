cd7f6901f9fed618230d000567752d0dd8df06d0
vadwwxz <vadwwxz@gmail.com>
2023-08-16 16:48:39 +0300

    remove unused data

--- app/src/main/kotlin/com/cinematica/backend/app/Application.kt
+++ app/src/main/kotlin/com/cinematica/backend/app/Application.kt
@@ -7,7 +7,6 @@ import com.cinematica.backend.app.dependencies.AppModule
 import com.cinematica.backend.app.services.monitoring.configureMonitoring
 import com.cinematica.backend.app.services.serialization.configureSerialization
 import com.cinematica.backend.domain.authorization.routing.configureAuthorizationRouting
-import com.cinematica.backend.domain.authorization.types.common.AuthorizationRequest
 import com.cinematica.backend.domain.authorization.usecases.signin.SignInUseCase
 import com.cinematica.backend.domain.authorization.usecases.signup.SignUpUseCase
 import com.cinematica.backend.domain.authorization.usecases.state.AuthorizationStateUseCase
@@ -15,14 +14,11 @@ import com.cinematica.backend.foundation.cli.asArguments
 import com.cinematica.backend.foundation.cli.getNamedIntOrNull
 import com.cinematica.backend.foundation.security.token.data.TokenConfig
 import com.typesafe.config.ConfigFactory
-import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.server.request.receiveNullable
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
-import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
@@ -75,36 +71,15 @@ fun main(args: Array<String>) {
     embeddedServer(Netty, port) {
         configureMonitoring()
         configureSerialization()
-//        configureAuthorizationRouting(
-//            signUpUseCase = koin.get<SignUpUseCase>(),
-//            signInUseCase = koin.get<SignInUseCase>(),
-//            authorizationStateUseCase = koin.get<AuthorizationStateUseCase>()
-//        )
+        configureAuthorizationRouting(
+            signUpUseCase = koin.get<SignUpUseCase>(),
+            signInUseCase = koin.get<SignInUseCase>(),
+            authorizationStateUseCase = koin.get<AuthorizationStateUseCase>()
+        )
         routing {
-            val signUpUseCase = koin.get<SignUpUseCase>()
             get("/hello") {
                 println("hello world")
                 call.respond("HELLO FUC*ING")
-            }
-            post("/auth/signup") {
-                println("sign up")
-                val authorizationRequest = kotlin.runCatching {
-                    call.receiveNullable<AuthorizationRequest>()
-                }.getOrNull() ?: kotlin.run {
-                    call.respond(HttpStatusCode.BadRequest)
-                    return@post
-                }
-
-                if (authorizationRequest.validationErrorMessage != null) {
-                    call.respond(authorizationRequest.validationErrorMessage.toString())
-                    return@post
-                }
-
-                val result = signUpUseCase.execute(authorizationRequest)
-
-                result.getOrNull()?.let { request ->
-                    call.respond(HttpStatusCode.OK, request)
-                } ?: call.respond(HttpStatusCode.BadRequest, result.exceptionOrNull()?.message.toString())
             }
         }
         println("Server started on address: http://127.0.0.1:$port")
