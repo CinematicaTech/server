da4d3920108a3a65cf715c16444c420aae27d8e7
vadwwxz <vadwwxz@gmail.com>
2023-09-02 09:13:53 +0300

    fix bug (method get cannot have a body), and i change it to queryParameters

--- domain/authorization/src/main/kotlin/com/cinematica/backend/domain/authorization/routing/state/AuthorizationStateRouting.kt
+++ domain/authorization/src/main/kotlin/com/cinematica/backend/domain/authorization/routing/state/AuthorizationStateRouting.kt
@@ -5,32 +5,32 @@ import com.cinematica.backend.domain.authorization.types.state.AuthorizationStat
 import com.cinematica.backend.domain.authorization.usecases.state.GetAuthorizationStateUseCase
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
-import io.ktor.server.request.receiveNullable
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.route
 
-fun Route.authorizationStateRouting(
-    getAuthorizationStateUseCase: GetAuthorizationStateUseCase
-) {
-    get("auth/state") {
-        val authorizationStateData = kotlin.runCatching {
-            call.receiveNullable<AuthorizationStateRequest>()
-        }.getOrNull() ?: kotlin.run {
-            call.respond(HttpStatusCode.BadRequest)
-            return@get
+fun Route.authorizationStateRouting(getAuthorizationStateUseCase: GetAuthorizationStateUseCase) {
+    route("auth/state") {
+        get {
+            val email = call.request.queryParameters["email"]
+
+            if (email.isNullOrBlank()) {
+                call.respond(HttpStatusCode.BadRequest, "Email parameter is missing or blank.")
+                return@get
+            }
+
+            val authorizationStateData = AuthorizationStateRequest(email = email)
+
+            if (authorizationStateData.validationErrorMessage != null) {
+                call.respond(HttpStatusCode.BadRequest, authorizationStateData.validationErrorMessage)
+                return@get
+            }
+
+            val authorizationState = getAuthorizationStateUseCase.execute(authorizationStateData)
+
+            val result = AuthorizationStateResponse(state = authorizationState.value)
+            call.respond(HttpStatusCode.OK, result)
         }
-
-        if (authorizationStateData.validationErrorMessage != null) {
-            call.respond(authorizationStateData.validationErrorMessage)
-            return@get
-        }
-
-        val authorizationState = getAuthorizationStateUseCase.execute(authorizationStateData)
-        val result = AuthorizationStateResponse(
-            state = authorizationState.value
-        )
-
-        call.respond(HttpStatusCode.OK, result)
     }
 }
